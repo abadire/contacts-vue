d715bf3f2d0bd9da6bb16b0e2407f7348bc47c16
abadire <panvan97@gmail.com>
2020-12-22 12:38:52 +0300

    feat: add header

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <header class="header">
+    {{state.heading}}
+  </header>
+</template>
+
+<script>
+export default {
+  props: {
+    heading: {
+      type: String,
+      required: true,
+    },
+  },
+  setup(props) {
+    const state = {
+      heading: props.heading,
+    };
+
+    return {
+      state,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
