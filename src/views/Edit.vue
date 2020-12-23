a824375ea3b0b5205a65534b155763d4a8b956a8
abadire <panvan97@gmail.com>
2020-12-23 17:52:41 +0300

    feat: start Edit page

---
+++ src/views/Edit.vue
@@ -0,0 +1,18 @@
+<template>
+  <Header heading="Edit"/>
+  <Overlay/>
+</template>
+
+<script>
+import Header from '../components/Header.vue';
+import Overlay from '../components/OverlayPopup.vue';
+
+export default {
+  name: 'Home',
+  components: { Header, Overlay },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
