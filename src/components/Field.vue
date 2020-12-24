fa8391e6463ba9ebae65744dff98899442cfdd69
abadire <panvan97@gmail.com>
2020-12-24 23:49:41 +0300

    feat: add field edition and auto-focus

--- src/components/Field.vue
+++ src/components/Field.vue
@@ -1,16 +1,47 @@
 <template>
     <div class="field">
-      <div class="field__name">{{name}}</div>
+      <span
+        type="text"
+        class="field__name"
+        ref="inputName"
+        :contenteditable="isEditable"
+        :class="{'field__name--editable': isEditable}"
+      >{{name}}</span>
       <span v-if="value" class="field__delim">:</span>
       <div v-if="value" class="field__value">Value</div>
       <div class="field__buttons">
-        <Button class="field__icons" v-if="value"  icon="delete_forever" type="icon-red"/>
-        <Button class="field__icons" icon="edit" type="icon-green"/>
+        <Button
+          class="field__icons"
+          v-if="value && !isEditable"
+          icon="delete_forever"
+          type="icon-red"
+        />
+        <Button
+          class="field__icons"
+          v-if="!isEditable"
+          icon="edit"
+          type="icon-green"
+          @click="toggleEdit"
+        />
+        <Button
+          class="field__icons"
+          v-if="isEditable"
+          icon="cancel"
+          type="icon-red"
+        />
+        <Button
+          class="field__icons"
+          v-if="isEditable"
+          icon="done"
+          type="icon-green"
+          @click="toggleEdit"
+        />
       </div>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue';
 import Button from './Button.vue';
 
 export default {
@@ -19,6 +50,28 @@ export default {
   props: {
     name: String,
     value: String,
+  },
+  setup() {
+    const isEditable = ref(false);
+    const inputName = ref(null);
+
+    function toggleEdit() {
+      isEditable.value = !isEditable.value;
+      setTimeout(() => {
+        inputName.value.focus();
+        const range = new Range();
+        range.setStart(inputName.value, 0);
+        range.setEnd(inputName.value, 1);
+        document.getSelection().empty();
+        document.getSelection().addRange(range);
+      }, 200);
+    }
+
+    return {
+      inputName,
+      isEditable,
+      toggleEdit,
+    };
   },
 };
 </script>
@@ -45,6 +98,23 @@ export default {
 
     &:hover {
       box-shadow: 0 .5rem 1rem #0003;
+    }
+
+    &__name {
+      background-color: $contact-background-light;
+      padding: .3rem;
+      border-radius: 2px;
+      border-bottom: 2px solid transparent;
+      transition: all .2s;
+
+      &--editable {
+        background-color: white;
+        border-bottom: 2px solid royalblue;
+      }
+
+      &:focus {
+        outline: none;
+      }
     }
 
     &__buttons {
